f2befb331e7aa8864019b344f14de16330535282
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2020-05-31 16:42:47 -0700

    fix: remove valid-jsdoc
    the rule has not kept up with the times and simply enabling it requires a whole bunch of things that just do not make sense anymore

--- src/configs/common/rules/errors.js
+++ src/configs/common/rules/errors.js
@@ -4,29 +4,5 @@ module.exports = {
   rules: {
     'no-extra-parens': 'error',
     'require-atomic-updates': 'error',
-    'valid-jsdoc': [
-      'error',
-      {
-        prefer: {
-          arg: 'param',
-          argument: 'param',
-          fires: 'emits',
-          return: 'returns',
-          virtual: 'abstract',
-        },
-        preferType: {
-          Boolean: 'boolean',
-          Number: 'number',
-          String: 'string',
-          function: 'Function',
-          object: 'Object',
-        },
-        requireParamDescription: false,
-        requireParamType: true,
-        requireReturn: false,
-        requireReturnDescription: false,
-        requireReturnType: true,
-      },
-    ],
   },
 };
